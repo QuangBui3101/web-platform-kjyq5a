85fa8a6ff79f21dcda9098070343bde545a9dcff
Nhat Quang Bui <nqbui@daenet.com>
2020-10-27 18:10:12 +0100

    update tags

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,5 @@
 const activeClass = "active";
+const activeTag = "activeTag";
 
 var codeParam = document.getElementById("SPInitParam");
 var codeConfig = document.getElementById("SPInitConfig");
@@ -7,46 +8,56 @@ var code = document.getElementsByClassName("code");
 var tags = document.getElementsByClassName("tag")[0].getElementsByTagName("a");
 CodeAction();
 
-function ClickedParam(element) 
-{
-    codeParam.classList.add(activeClass);
-    codeConfig.classList.remove(activeClass);
-    CodeAction();
+function ClickedParam(element) {
+  DeactivateTags(tags);
+  element.classList.add(activeTag);
+  codeParam.classList.add(activeClass);
+  codeConfig.classList.remove(activeClass);
+  CodeAction(element);
 }
 
-function ClickedConfig(element) 
-{
-    codeParam.classList.remove(activeClass);
-    codeConfig.classList.add(activeClass);
-    CodeAction();
+function ClickedConfig(element) {
+  DeactivateTags(tags);
+  element.classList.add(activeTag);
+  codeParam.classList.remove(activeClass);
+  codeConfig.classList.add(activeClass);
+  CodeAction(element);
 }
 
 function Show(node) {
-    node.style.height = "auto";
-    node.style.opacity = "100%";    
+  node.style.height = "auto";
+  node.style.opacity = "100%";
 }
 
 function Hide(node) {
-    node.style.height = null;
-    node.style.opacity = null;
+  node.style.height = null;
+  node.style.opacity = null;
 }
 
-function CodeAction() {
-    for (let i = 0; i < code.length; i++) {
-        const node = code[i];
-        if (node.classList.contains("active")) {
-            Show(node);
-        }
-        else {
-            Hide(node);
-        }
+function CodeAction(element) {
+  if (element == null) {
+    ActiveTag(tags[0]);
+  } else {
+    ActiveTag(element);
+  }
+  for (let i = 0; i < code.length; i++) {
+    const node = code[i];
+    if (node.classList.contains("active")) {
+      Show(node);
+    } else {
+      Hide(node);
     }
+  }
 }
 
 function ActiveTag(element) {
-    element.style.borderTop = "thick solid #7014e8";
+  element.style.borderTop = "thick solid #7014e8";
 }
 
 function DeactivateTags(elements) {
-    
+  for (let i = 0; i < elements.length; i++) {
+    const tag = elements[i];
+    tag.classList.remove(activeTag);
+    tag.style.borderTop = null;
+  }
 }
